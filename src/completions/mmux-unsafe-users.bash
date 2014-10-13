07663b7b373a7395764bd0a2486097077688170c
Marco Maggi <marco.maggi-ipsu@poste.it>
2014-10-13 16:02:28 +0200

    development

--- src/completions/mmux-unsafe-users.bash
+++ src/completions/mmux-unsafe-users.bash
@@ -1,11 +1,12 @@
 # unsafeusers.bash --
 #
-# Completions for the "unsafe-users" script.
+# Completions for the "mmux-unsafe-users" script.
 
-complete -F p-unsafe-users unsafe-users
-function p-unsafe-users () {
+complete -F p-mmux-unsafe-users /usr/sbin/mmux-unsafe-users
+complete -F p-mmux-unsafe-users mmux-unsafe-users
+function p-mmux-unsafe-users () {
     local word_to_be_completed=${COMP_WORDS[${COMP_CWORD}]}
-    # COMP_CWORD is zero based.  Index 0 is the "unsafe-users" word.
+    # COMP_CWORD is zero based.  Index 0 is the "mmux-unsafe-users" word.
     case "$COMP_CWORD" in
         1)
             local first_word_completions='add del bind unbind enable-x disable-x help'
