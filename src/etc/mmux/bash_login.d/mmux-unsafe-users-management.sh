210372e5d88aad65425e621492897358e9cdb26b
Marco Maggi <marco.maggi-ipsu@poste.it>
2014-10-28 06:57:18 +0100

    fixed login and logout scripts, comments and error messages

--- src/etc/mmux/bash_login.d/mmux-unsafe-users-management.sh
+++ src/etc/mmux/bash_login.d/mmux-unsafe-users-management.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 # /etc/mmux/bash_login.d/mmux-unsafe-users-management.sh --
 #
-# Login script.  Sets up support for unsafe user accounts managed
+# Login script, to be executed  at user login-time by "~/.bash_login" or
+# similar scripts.  Sets up support for unsafe user accounts managed
 # by the package MMUX Unsafe Users Management.
 
 SCRIPT=/usr/sbin/mmux-unsafe-users-manager
 
 if test -x "$SCRIPT"
 then "$SCRIPT" bind --verbose
-else printf '%s: script file not found: rc.unsafe-users\n' "$0" "$SCRIPT" >&2
+else printf '%s: script file not found: %s\n' "$0" "$SCRIPT" >&2
 fi
 
 ### end of file
--- src/etc/mmux/bash_logout.d/mmux-unsafe-users-management.sh
+++ src/etc/mmux/bash_logout.d/mmux-unsafe-users-management.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 # /etc/mmux/bash_logout.d/mmux-unsafe-users-management.sh --
 #
-# Logout script.  Sets up support for unsafe user accounts managed
-# by the package MMUX Unsafe Users Management.
+# Logout script, to be executed  at user logout-time by "~/.bash_logout"
+# or  similar  scripts.  Sets  down  support  for unsafe  user  accounts
+# managed by the package MMUX Unsafe Users Management.
 
 SCRIPT=/usr/sbin/mmux-unsafe-users-manager
 
 if test -x "$SCRIPT"
 then "$SCRIPT" unbind --verbose
-else printf '%s: script file not found: rc.unsafe-users\n' "$0" "$SCRIPT" >&2
+else printf '%s: script file not found: %s\n' "$0" "$SCRIPT" >&2
 fi
 
 ### end of file
